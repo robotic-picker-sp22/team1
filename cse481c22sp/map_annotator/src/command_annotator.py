08d25c6dbe7d43d0e1411e117e593f9207ea7739
Jakub Filipek <balbok@cs.washington.edu>
2022-04-17 22:32:03 -0700

    Fix some issues with command line. Cannot get goto region to work

--- cse481c22sp/map_annotator/src/command_annotator.py
+++ cse481c22sp/map_annotator/src/command_annotator.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 from typing import Dict, Optional
 import rospy
-from geometry_msgs.msg import Pose
-from nav_msgs.msg import Odometry
+from geometry_msgs.msg import PoseWithCovarianceStamped, PoseStamped
+from nav_msgs.srv import GetPlan, GetPlanRequest
 from multiprocessing import Lock
 import json
+import textwrap
 
 
 class CommandAnnotator():
@@ -16,25 +17,57 @@ class CommandAnnotator():
             "help": self.callback_help,
             "goto": self.callback_goto,
         }
-        self.poses_dict: Dict[str, Pose] = {}
-        self.cur_pose: Optional[Pose] = None
+        self.poses_dict: Dict[str, PoseStamped] = {}
+        self.cur_pose: Optional[PoseStamped] = None
         self.pose_lock = Lock()
 
         self.__SAVE_DIR = Path("tmp_annotator_data/")
+
+        if not self.__SAVE_DIR.exists():
+            self.__SAVE_DIR.mkdir(parents=True)
 
         # Load saved poses
         for path in self.__SAVE_DIR.glob("annotated_pose_*.json"):
             name = path.stem.split("_")[-1]
             with open(path, "r") as f:
                 pose_dict = json.load(f)
-                pose = Pose(**pose_dict)
+                pose = self.__dict_to_pose(pose_dict)
                 self.poses_dict[name] = pose
 
-        rospy.Subscriber("/odom", Odometry, self.rospy_callback_odom)
+        rospy.Subscriber("/amcl_pose", PoseWithCovarianceStamped, self.rospy_callback_odom)
 
-    def rospy_callback_odom(self, msg: Odometry):
+        rospy.wait_for_service("/move_base/NavfnROS/make_plan")
+        self.__send_goal_request = rospy.ServiceProxy("/move_base/NavfnROS/make_plan", GetPlan)
+        self.simple_pub = rospy.Publisher("/move_base_simple/goal", PoseStamped, queue_size=1)
+
+    @staticmethod
+    def __pose_to_dict(pose: PoseStamped) -> Dict[str, Dict[str, float]]:
+        result = {}
+        for slot in pose.__slots__:
+            slot_val = getattr(pose, slot)
+            if isinstance(slot_val, rospy.AnyMsg) or {"__slots__", "_slot_types"}.issubset(dir(slot_val)):
+                result[slot] = __class__.__pose_to_dict(slot_val)
+            else:
+                result[slot] = slot_val
+        return result
+
+    @staticmethod
+    def __dict_to_pose(pose_dict: Dict[str, Dict[str, float]]) -> PoseStamped:
+        result = PoseStamped()
+        for k, v in pose_dict.get("header", {}).items():
+            setattr(result.header, k, v)
+        for k, v in pose_dict.get("position", {}).items():
+            setattr(result.pose.position, k, v)
+        for k, v in pose_dict.get("orientation", {}).items():
+            setattr(result.pose.orientation, k, v)
+        return result
+
+    def rospy_callback_odom(self, msg: PoseWithCovarianceStamped):
         with self.pose_lock:
-            self.cur_pose = msg.pose.pose
+            tmp = PoseStamped
+            tmp.pose = msg.pose.pose
+            tmp.header = msg.header
+            self.cur_pose = tmp
 
     def callback_list(self):
         if len(self.poses_dict) == 0:
@@ -48,13 +81,10 @@ class CommandAnnotator():
         if self.cur_pose is not None:
             with self.pose_lock:
                 self.poses_dict[name] = self.cur_pose
-                cur_pose_dict = {
-                    slot: getattr(self.cur_pose, slot)
-                    for slot in self.cur_pose.__slots__
-                }
+                # Save pose
+                cur_pose_dict = self.__pose_to_dict(self.cur_pose)
                 with open(Path(self.__SAVE_DIR) / f"annotated_pose_{name}.json", "w") as f:
                     json.dump(cur_pose_dict, f)
-                    f.write(str(self.cur_pose))
         else:
             print("No pose to save. Is the robot running?")
 
@@ -63,20 +93,23 @@ class CommandAnnotator():
             print("No pose named {} to delete.".format(name))
 
     def callback_help(self):
-        print(
-            """Commands:
-                list: List saved poses.
-                save <name>: Save the robot's current pose as <name>. Overwrites if <name> already exists.
-                delete <name>: Delete the pose given by <name>.
-                goto <name>: Sends the robot to the pose given by <name>.
-                help: Show this list of commands
-                quit: Exit the program"""
-        )
+        print(textwrap.dedent(
+            """
+            Commands:
+             - list: List saved poses.
+             - save <name>: Save the robot's current pose as <name>. Overwrites if <name> already exists.
+             - delete <name>: Delete the pose given by <name>.
+             - goto <name>: Sends the robot to the pose given by <name>.
+             - help: Show this list of commands
+             - quit: Exit the program"""
+        ))
 
     def callback_goto(self, name: str):
         if name in self.poses_dict:
             print("Sending robot to {}".format(name))
-            self.poses_dict[name]
+            req = GetPlanRequest(start=self.cur_pose, goal=self.poses_dict[name])
+            self.__send_goal_request(req)
+            # self.simple_pub.publish(self.poses_dict[name])
         else:
             print("No pose named {} to goto.".format(name))
 
@@ -89,6 +122,7 @@ class CommandAnnotator():
                 break
 
             if command in self.command_dict:
+                # try:
                 if len(args.split(" ")) > 1:
                     self.command_dict[command](args.split(" ", 1)[1])
                 else:
@@ -96,12 +130,11 @@ class CommandAnnotator():
 
     def run(self) -> None:
         print(
-            """
-            Welcome to the map annotator!
-            """
+            """Welcome to the map annotator!"""
         )
         self.callback_help()
         self.spin()
 
 if __name__ == "__main__":
+    rospy.init_node('keyboard_map_annotator', anonymous=True)
     CommandAnnotator().run()
