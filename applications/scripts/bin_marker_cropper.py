562ddc7f8fe969ed81f755327bb63e2e87ab572d
capstone <suz8@cs.uw.edu>
2022-05-03 17:31:30 -0700

    bin_marker_cropper

--- applications/scripts/bin_marker_cropper.py
+++ applications/scripts/bin_marker_cropper.py
@@ -1,6 +1,5 @@
 #! /usr/bin/env python
 
-import rosbag
 import rospy
 from ar_track_alvar_msgs.msg import AlvarMarkers
 
@@ -20,37 +19,23 @@ class ArTagReader(object):
 
     def callback(self, msg):
         self.markers = msg.markers
+        if self.markers:
+            marker = next(filter(lambda m: m.id == 0, self.markers))  # maybe support cropping multiple markers
+            marker_position = marker.pose.pose.position
+            rospy.set_param("crop_min_x", marker_position.x)
+            rospy.set_param("crop_min_y", marker_position.y - 0.26)
+            rospy.set_param("crop_min_z", marker_position.z - 0.15)
+            rospy.set_param("crop_max_x", marker_position.x + 0.3)
+            rospy.set_param("crop_max_y", marker_position.y - 0.12)
+            rospy.set_param("crop_max_z", marker_position.z - 0.02)
 
 
 def main():
-    rospy.init_node("hallucinations")
+    rospy.init_node("bin_marker_cropper")
     wait_for_time()
 
     reader = ArTagReader()
     sub = rospy.Subscriber("/ar_pose_marker", AlvarMarkers, callback=reader.callback, queue_size=10)
-
-    if HALLUCINATE:
-        fake_topic_name = "/ar_pose_marker_fake"
-        fake_pub = rospy.Publisher(fake_topic_name, AlvarMarkers, queue_size=10)
-        # rospy.sleep(0.2)
-        rospy.Subscriber(fake_topic_name, AlvarMarkers, callback=reader.callback, queue_size=10)
-        # rospy.sleep(0.2)
-        bag = rosbag.Bag("/home/capstone/data/ar_pose_markers_example.bag")
-        for _, msg, _ in bag.read_messages(["/ar_pose_marker"]):
-            rospy.loginfo("Pub msg")
-            fake_pub.publish(msg)
-
-    while len(reader.markers) == 0:
-        rospy.sleep(0.1)
-
-    marker = next(filter(lambda m: m.id == 0, reader.markers))  # maybe support cropping multiple markers
-    marker_position = marker.pose.pose.position
-    rospy.set_param("crop_min_x", marker_position.x)
-    rospy.set_param("crop_min_y", marker_position.y - 0.26)
-    rospy.set_param("crop_min_z", marker_position.z - 0.15)
-    rospy.set_param("crop_max_x", marker_position.x + 0.3)
-    rospy.set_param("crop_max_y", marker_position.y - 0.12)
-    rospy.set_param("crop_max_z", marker_position.z - 0.02)
 
 
 if __name__ == '__main__':
